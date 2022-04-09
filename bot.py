5fb6377d8bbbcd63bf676ce2769f943f5520a679
Gabrielle Co <gabriellejco@example.com>
2022-04-09 14:06:52 -0500

    starter code

---
+++ bot.py
@@ -0,0 +1,13 @@
+import tweepy
+from config import consumer_key, consumer_secret, access_token, access_token_secret, bearer_token
+
+#Info to connect our API to our code
+client = tweepy.Client(consumer_key = consumer_key,
+                    consumer_secret = consumer_secret,
+                    access_token = access_token,
+                    access_token_secret = access_token_secret,
+                    bearer_token= bearer_token)
+
+
+#Creating a simple tweet
+response = client.create_tweet(text='bing bong3')
