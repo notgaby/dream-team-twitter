0601afafe89d8564d735f64f4512d3cdefae378c
Anthony Polka <anthonypolka@gmail.com>
2022-05-15 21:02:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,6 @@ client = tweepy.Client(consumer_key = consumer_key,
                     bearer_token= bearer_token)
 
 
-#Creating a simple tweet
-#response = client.create_tweet(text='Houston, we have a bot!!!!')
-
-
 print ("Program Start : %s" % time.ctime()) #time the program starts
 startDate = DT.datetime.now() 
 check = startDate.replace(minute=30)
